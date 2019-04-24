403a0020c68999699c3010a5a1bd2e5964966202
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-04-23 21:42:59 -0500

    Created React App.

---
+++ expat-journal/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>This is the Expat Journal!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
