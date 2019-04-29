1596065ad08c95016262845ff1ff9635b24574f5
shayaanakbar <shayaanakbar1106@gmail.com>
2019-04-29 13:11:49 -0500

    Added sign up

--- expat-journal/src/Components/Header/SignUp.js
+++ expat-journal/src/Components/Header/SignUp.js
@@ -1,13 +1,68 @@
 import React, { Component } from "react";
+import InputField from "./Input";
+import Button from "../Button";
+
+
 
 class SignUp extends Component {
-    render() {
-        return (
-            <div className={"sign-up"}>
-                <button>Sign Up!</button>
-            </div>
-        )
+    state = {
+        username: "",
+        password: "",
+        token: ""
     }
+
+  onChangeHandler = (e) => {
+    const {name, value} = e.target;
+    this.setState({[name]: value});
+  };
+
+  loginHandler = () => {
+    localStorage.setItem('token', JSON.stringify(this.matchCredentials(this.state.username, this.state.password)));
+  };
+
+  matchCredentials = (username, password) => {
+    const usernameToMatch = "123@gmail.com";
+    const passwordToMatch = "123123";
+    const token = "18238127371823172";
+
+    const isValid = (username === usernameToMatch.toLowerCase() && password === passwordToMatch.toLowerCase());
+
+    if (isValid) {
+      return token;
+    } else {
+      return "Error";
+    }
+  };
+
+  registerHandler = () => {
+    console.log(this.state);
+  };
+
+
+  render() {
+    const {username, password} = this.state;
+
+    return (
+      <div>
+        <InputField
+          name="username"
+          value={username}
+          placeholder="Username"
+          type="text"
+          onChange={this.onChangeHandler}
+        />
+        <InputField
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          onChange={this.onChangeHandler}
+        />
+        <Button text="Login" onClick={this.loginHandler}/>
+        <Button text="Register" onClick={this.registerHandler}/>
+      </div>
+    )
+  }
 };
 
 export default SignUp;
