f89a56fe265c8876498384a2a4b437da56346615
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-01 22:12:32 -0500

    Fixed post figuring out how to attach to backend

--- expat-journal/src/Components/Header/AuthenticationGroup.js
+++ expat-journal/src/Components/Header/AuthenticationGroup.js
@@ -18,15 +18,22 @@ class AuthenticationGroup extends React.Component {
       };
     }
 
-    loginHandler = () => {
+    loginHandler = (e) => {
+      e.preventDefault()
       console.log('Login Successful!')
-      let user = {"username": this.state.username, "password": this.state.password}
+      let user = {username: this.state.username, password: this.state.password}
       console.log(user)
       axios
         .post(`https://expat-lambda.herokuapp.com/api/login`, user)
         .then(response => {
           console.log("LOGIN RESPONSE", response.data)
           localStorage.setItem('token', (response.data.token));
+          const token= localStorage.getItem('jwt');
+          const reqOptions = {
+            headers:{
+              Authorization:token
+            }
+          }
         })
         .catch(err => console.error('login error:', err))
     }
@@ -37,23 +44,26 @@ class AuthenticationGroup extends React.Component {
         this.setState({[name]: value});
     }
 
-    registerHandler = (e) => {
-      console.log(this.state);
-    };
-
 
     render() {
 
         return (
             <div>
+                <form onSubmit={this.loginHandler}>
                 <InputField
                   name="username"
                   placeholder="Username"
                   type="text"
                   onChange={this.onChangeHandler}
                 />
-                <Button text="Login" onClick={this.loginHandler}/>
-                {/*<Button text="Register" onClick={this.registerHandler}/>*/}
+                  <InputField
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                    onChange={this.onChangeHandler}
+                  />
+                <Button type={"submit"} text="Login"/>
+                </form>
             </div>
         )
     }
--- expat-journal/src/Components/Header/SignUp.js
+++ expat-journal/src/Components/Header/SignUp.js
@@ -18,13 +18,21 @@ class SignUp extends React.Component {
 
 
 
-  registerHandler = id => {
+  registerHandler = (e) => {
+    e.preventDefault()
     console.log('Sign Up Successful!')
+    let user = {"username": this.state.username, "password": this.state.password}
     axios
-      .post(`https://expat-lambda.herokuapp.com/api/registration${id}`)
+      .post(`https://expat-lambda.herokuapp.com/api/registration`, user)
       .then(response => {
         console.log("SIGNUP RESPONSE", response.data)
         localStorage.setItem('token', (response.data.token));
+        const token= localStorage.getItem('jwt');
+        const reqOptions = {
+          headers:{
+            Authorization:token
+          }
+        }
       })
       .catch(err => console.error("signup error:", err));
   }
@@ -44,6 +52,12 @@ class SignUp extends React.Component {
   render() {
     return (
       <div>
+        <InputField
+          name="username"
+          placeholder="Username"
+          type="text"
+          onChange={this.onChangeHandler}
+        />
         <InputField
           name="password"
           placeholder="Password"
